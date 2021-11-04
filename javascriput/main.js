56c3f46b48360eb04dd3f42486ff7a2d6f0660b0
alexis <mbonigabalexis624@gmail.com>
2021-11-04 17:42:28 +0200

    myfirst comment

--- javascriput/main.js
+++ javascriput/main.js
@@ -1,70 +1,57 @@
-/* <li class="listiterm">
 
 
-        <div class="personal-info">
-      <h2>Ndayambaje Gaetan</h2>
-      <p>gaetandeo@gmail.com</p>
-      <p>0782989299</p>
-       </div>
-       <div class="actions">
-        <button >Remove Student</button>
-       </div> 
-       </li> */
 
 
-       const student=[
-         {
-           name:"Ndayambaje gaetan",
-           email:"gaetandeo@gmail.com",
-           phone:"078328992",
-           address:"GATSIBO"
-
-       },
-       {
-         name:"Innocent Ishimwe",
-         email:"ishimwe@gmail.com",
-         phone:"07882992",
-         address:"GATSIBO"
-       },
-       {
-         name:"Mbonigaba alexis",
-         email:"mbonigabalexis624@gmail.com",
-         phone:"0780204570",
-         address:"GATSIBO"
+       const students=JSON.parse(localStorage.getItem("students"))||[]; //step6
+       // 1. get data from from
+       // 2. store data in student object
+       // 3. push OBJECT into student array
+       //4.store student  array int local storage
+       //5.refref
+       //// 6 all thes operation will be done in onclick  event  
 
 
-       },
-       {
-        name:" Birori patrick",
-        email:"biroripatrick@gmail.com",
-        phone:"078899777878",
-        address:"GATSIBO"
+
+// to show div hideon hiddeen
+       document.getElementById("btn-add-student").addEventListener('click',function(e){
+      document.getElementById("form-container").classList.remove("hideorshow")
+ 
+       });
+
+       // close div class hideon hiden
+
+       document.getElementById("btnclose").addEventListener('click',function(e){
+       e.preventDefault();
+       document.getElementById("form-container").classList.add("hideorshow");
+
+       });
+       
+       document.getElementById("submitdata").addEventListener('click',function(e){ //step1
+              e.preventDefault();
+
+              let name=document.getElementById("name").value;
+              let email=document.getElementById("email").value;
+              let phone=document.getElementById("phone").value;
+              let address=document.getElementById("address").value;
+              let id=document.getElementById("id").value;
+
+              let student={name,email,phone,address,id};// step2
+              students.push(student);// step3
+              localStorage.setItem("students",JSON.stringify(students)); // step4
+              location.reload();//step5
+              
+              
+              
 
 
-      },
-      {
-        name:"Rutabara Vagne",
-        email:"rutabaravagne@gmail.com",
-        phone:"07879544444",
-        address:"GATSIBO"
-
-      },
-      {
-        name:"Muhuza Huseun",
-        email:"muhuzahueseun@gmail.com",
-        phone:"0780204570",
-        address:"GATSIBO"
-
-
-      }
-      ];
+       });
 
       // using for loop dispay tag and information in html page
 
       let  studentlist=document.getElementById("studentlist");
       
       
-      for(let i=0;i<student.length;i++){
+      for(let i=0;i<students.length;i++){
 
         let listiterm=document.createElement('li');
             listiterm.classList.add("listitem");
@@ -76,14 +63,16 @@
        let studentEmail=document.createElement('p');
        let studentPhone=document.createElement('p');
        let studentAddress=document.createElement('p');
+       let studentId=document.createElement('b');
        let removeStudent=document.createElement('button');
 // <button> remove student
 // set data to tag calling in html
        removeStudent.innerHTML="Remove student";
-       studentName.innerHTML= student[i].name;
-       studentEmail.innerHTML=student[i].email;
-       studentPhone.innerHTML=student[i].phone;
-       studentAddress.innerHTML=student[i].address;
+       studentName.innerHTML= students[i].name;
+       studentEmail.innerHTML=students[i].email;
+       studentPhone.innerHTML=students[i].phone;
+       studentAddress.innerHTML=students[i].address;
+       studentId.innerHTML=students[i].id;
 
        // append tags to their parents
        
@@ -91,6 +80,7 @@
       personalInfo.appendChild(studentEmail);
       personalInfo.appendChild(studentPhone);
       personalInfo.appendChild(studentAddress);
+      personalInfo.appendChild(studentId);
       
       
       actions.appendChild(removeStudent);
@@ -98,6 +88,6 @@
       listiterm.appendChild(personalInfo);
       listiterm.appendChild(actions);
       studentlist.appendChild(listiterm);
-
-
       }
+
+      
